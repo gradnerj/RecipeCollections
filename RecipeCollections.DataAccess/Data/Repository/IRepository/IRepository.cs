90d8cfbd24398304763cca340e8ef6dc2d1a522f
Jeff <gradnerj@gmail.com>
2020-09-21 14:19:20 -0600

    Added IRepository

---
+++ RecipeCollections.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace RecipeCollections.DataAccess.Data.Repository.IRepository {
+    public interface IRepository<T> where T : class {
+        T Get(int id);
+
+        //Get All Objects
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            //Comma Delimited String
+            string includeProperties = null
+        );
+
+        //Get the First or Default
+        T GetFirstorDefault(
+            Expression<Func<T, bool>> filter = null,
+            //Comma Delimited String
+            string includeProperties = null
+        );
+        //Add 
+        void Add(T entity);
+
+        //Remove by Id
+        void Remove(int id);
+
+        //Remove by Object
+        void Remove(T entity);
+
+        //Remove a List of Objects
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
