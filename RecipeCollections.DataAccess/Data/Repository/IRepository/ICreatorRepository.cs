d7fe08a95c27d2023e7246b13eddf04baa16686e
Jeff <gradnerj@gmail.com>
2020-09-21 14:32:36 -0600

    Added interface repositories and classes for each model

---
+++ RecipeCollections.DataAccess/Data/Repository/IRepository/ICreatorRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeCollections.DataAccess.Data.Repository.IRepository {
+    public interface ICreatorRepository {
+    }
+}
