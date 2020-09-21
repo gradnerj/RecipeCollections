6fc461acc0634ccf89fe12d179c23f00e091a403
Jeff <gradnerj@gmail.com>
2020-09-21 10:21:58 -0700

    Moved Data and ApplicationDbContext to DataAccess dll

--- RecipeCollections.DataAccess/Data/ApplicationDbContext.cs
+++ RecipeCollections.DataAccess/Data/ApplicationDbContext.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 
 namespace RecipeCollections.Data
 {
