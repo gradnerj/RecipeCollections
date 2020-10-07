3baaff363d40f03b827e8d967a062b43db228965
Jeff <gradnerj@gmail.com>
2020-10-06 20:51:26 -0600

    Updated Admin Recipe CRUD pages for refactored models

--- RecipeCollections/Pages/Admin/Recipe/Delete.cshtml.cs
+++ RecipeCollections/Pages/Admin/Recipe/Delete.cshtml.cs
@@ -1,14 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using RecipeCollections.Data;
+using RecipeCollections.Models;
 
-namespace RecipeCollections.Pages.Admin.Recipe {
+namespace RecipeCollections.Pages.Admin.Recipe
+{
     public class DeleteModel : PageModel
     {
-        private readonly Data.ApplicationDbContext _context;
+        private readonly RecipeCollections.Data.ApplicationDbContext _context;
 
-        public DeleteModel(Data.ApplicationDbContext context)
+        public DeleteModel(RecipeCollections.Data.ApplicationDbContext context)
         {
             _context = context;
         }
--- RecipeCollections/Pages/Admin/Recipe/Index.cshtml.cs
+++ RecipeCollections/Pages/Admin/Recipe/Index.cshtml.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using RecipeCollections.Data;
+using RecipeCollections.Models;
 
-namespace RecipeCollections.Pages.Admin.Recipe {
+namespace RecipeCollections.Pages.Admin.Recipe
+{
     public class IndexModel : PageModel
     {
-        private readonly Data.ApplicationDbContext _context;
+        private readonly RecipeCollections.Data.ApplicationDbContext _context;
 
-        public IndexModel(Data.ApplicationDbContext context)
+        public IndexModel(RecipeCollections.Data.ApplicationDbContext context)
         {
             _context = context;
         }
