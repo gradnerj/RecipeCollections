41cdf541251ddb73b99859ad82df6e2f8bb3c2ce
Jeff <gradnerj@gmail.com>
2020-10-06 20:20:13 -0600

    Removed Step Utensil and Creator Models

--- RecipeCollections.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ RecipeCollections.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -3,12 +3,9 @@
 namespace RecipeCollections.DataAccess.Data.Repository.IRepository {
     public interface IUnitOfWork : IDisposable {
 
-        ICreatorRepository Creator { get; }
         IRecipeRepository Recipe { get; }
         IIngredientRepository Ingredient { get; }
         IReviewRepository Review { get; }
-        IStepRepository Step { get; }
-        IUntensilRepository Utensil { get; }
 
         IApplicationUserRepository ApplicationUser { get; }
         void Save();
