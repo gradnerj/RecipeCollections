57a305d2280937976b3ce5fea53881a4db366b42
Jeff <gradnerj@gmail.com>
2020-10-06 20:23:38 -0600

    Removed utensil step and creator, removed from context

--- RecipeCollections.DataAccess/Data/ApplicationDbContext.cs
+++ RecipeCollections.DataAccess/Data/ApplicationDbContext.cs
@@ -10,11 +10,11 @@ namespace RecipeCollections.Data
             : base(options)
         {
         }
-        public DbSet<Creator> Creators { get; set; }
+        
         public DbSet<Recipe> Recipes { get; set; }
-        public DbSet<Step> Steps { get; set; }
+        
         public DbSet<Ingredient> Ingredients { get; set; }
-        public DbSet<Utensil> Utensils { get; set; }
+        
         public DbSet<Review> Reviews { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
