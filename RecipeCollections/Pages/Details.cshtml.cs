6c178518e5c887f5c70af7e294272836293108aa
Jeff <gradnerj@gmail.com>
2020-10-13 12:24:19 -0600

    Details page handles updating two cookies in session

---
+++ RecipeCollections/Pages/Details.cshtml.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeCollections.Data;
+using RecipeCollections.Utility;
+
+namespace RecipeCollections.Pages {
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public DetailsModel(ApplicationDbContext context) {
+            _context = context;
+        }
+        public Models.Recipe Recipe { get; set; }
+        public void OnGet(int id)
+        {
+            List<int> recipeid_list = null;
+            int recent_count = 0;
+            //Empty session
+            if (HttpContext.Session.Get<List<int>>(StaticDetails.RecentlyViewed) == default) {
+                recipeid_list = new List<int>();
+                
+            } else {
+                recipeid_list = HttpContext.Session.Get<List<int>>(StaticDetails.RecentlyViewed);
+            }
+            recent_count = HttpContext.Session.Get<int>(StaticDetails.RecentlyViewedCount);
+            if (!recipeid_list.Exists(rId => rId == id)) {
+                recipeid_list.Add(id);
+                recent_count++;
+            }
+            
+
+            HttpContext.Session.Set(StaticDetails.RecentlyViewed, recipeid_list);
+            HttpContext.Session.Set(StaticDetails.RecentlyViewedCount, recent_count);
+            Recipe = _context.Recipes.Where(r => r.Id == id).FirstOrDefault();
+        }
+    }
+}
