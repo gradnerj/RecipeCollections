46cda81ab04b6f33f881060fe0d82c3fcf69816e
Jeff <gradnerj@gmail.com>
2020-10-13 08:56:07 -0600

    Migrated updated RecentlyViewed Model

---
+++ RecipeCollections.DataAccess/Migrations/20201013145533_AddedCountToRecentlyViewedModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeCollections.DataAccess.Migrations
+{
+    public partial class AddedCountToRecentlyViewedModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "RecentlyViewed",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "RecentlyViewed");
+        }
+    }
+}
